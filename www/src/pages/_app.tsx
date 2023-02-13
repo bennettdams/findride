7d3ef9c978452bd0b4faaf175731a0a37de8887e
Bennett Dams <dams.bennett@gmail.com>
2023-02-13 18:14:11 +0100

    Add head title

--- www/src/pages/_app.tsx
+++ www/src/pages/_app.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css'
 import { Inter } from '@next/font/google'
 import { Analytics } from '@vercel/analytics/react'
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 
 const globalFont = Inter({
   subsets: ['latin'],
@@ -11,11 +12,18 @@ const globalFont = Inter({
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <main
-      className={`${globalFont.className} w-full bg-dsecondary text-dsecondary h-screen`}
-    >
-      <Analytics />
-      <Component {...pageProps} />
-    </main>
+    <>
+      <Head>
+        <title>FindRide</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main
+        className={`${globalFont.className} w-full bg-dsecondary text-dsecondary h-screen`}
+      >
+        <Analytics />
+        <Component {...pageProps} />
+      </main>
+    </>
   )
 }
