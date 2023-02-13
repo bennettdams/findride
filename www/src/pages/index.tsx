09df879fb0627a51768fd2a6af51133d0bcd9e52
Bennett Dams <dams.bennett@gmail.com>
2023-02-13 18:06:29 +0100

    Add FindRide favicon | Home page styling

--- www/src/pages/index.tsx
+++ www/src/pages/index.tsx
@@ -10,14 +10,17 @@ export default function Home() {
           </h2>
         </div>
 
-        <div className="row-span-1 grid place-items-center">
+        <div className="row-span-1 grid place-items-center items-start">
           <div className="bg-dtertiary mx-10 shadow-md p-10 rounded-lg hover:bg-red-100">
             <h2 className="text-4xl text-dsecondary">
               Hier entsteht eine Webpräsenz.
             </h2>
           </div>
 
-          <a className="underline" href="mailto:hi@findride.de">
+          <a
+            className="underline text-xl hover:text-dprimary"
+            href="mailto:hi@findride.de"
+          >
             hi@findride.de
           </a>
         </div>
