f28e7be48cad73e4424faa5d387c94127bab61cd
Bennett Dams <dams.bennett@gmail.com>
2023-02-13 15:15:25 +0100

    Initial home page

--- www/src/pages/_app.tsx
+++ www/src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
+import { Nunito } from '@next/font/google'
 import type { AppProps } from 'next/app'
 
+const globalFont = Nunito({
+  subsets: ['latin'],
+  weight: ['400', '500', '600', '800'],
+  variable: '--global-condun-font',
+  display: 'swap',
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <main className={`${globalFont.className} w-full h-screen`}>
+      <Component {...pageProps} />
+    </main>
+  )
 }
